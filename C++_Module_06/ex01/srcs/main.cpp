95389b1aaf5343878057eee8f6da75f0470692cb
ecnaraga <garance.lambey@gmail.com>
2024-03-21 12:20:58 +0100

    cpp06 ex01

---
+++ C++_Module_06/ex01/srcs/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: garance <garance@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 11:59:11 by garance           #+#    #+#             */
+/*   Updated: 2024/03/21 12:18:35 by garance          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Data.hpp"
+#include "../includes/Serializer.hpp"
+#include <iostream>
+
+int main(void) {
+
+    std::string title = "\e[34m";    
+    std::string reset = "\e[0m";
+        
+    //Avec un pointeur valide
+    Data p;
+    uintptr_t ptr_dec = Serializer::serialize(&p);
+    Data *ptr = Serializer::deserialize(ptr_dec);
+
+    std::cout << std::endl << title << "Avec un pointeur non NULL" << reset << std::endl;
+    std::cout << "Adresse de p : " << &p << std::endl;
+    std::cout << "Adresse de p convertie en uintprt_t : " << ptr_dec << std::endl;
+    std::cout << "uintptr convertie en Data * : " << ptr << std::endl;
+
+    //Avec NULL
+    std::cout << std::endl << title << "Avec un pointeur NULL" << reset << std::endl;
+    ptr_dec = Serializer::serialize(NULL);
+    ptr = Serializer::deserialize(ptr_dec);
+    std::cout << "Pointeur NULL converti en uintprt_t : " << ptr_dec << std::endl;
+    std::cout << "uintptr convertie en Data * : " << ptr << std::endl;
+    
+    return (0);
+}
