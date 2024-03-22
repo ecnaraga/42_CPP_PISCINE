b00f52f099a25aca714090026ca549075a96e96a
gaga <garance.lambey@gmail.com>
2024-03-22 14:20:26 +0100

    p

--- C++_Module_07/ex00/srcs/main.cpp
+++ C++_Module_07/ex00/srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: garance <garance@student.42.fr>            +#+  +:+       +#+        */
+/*   By: galambey <galambey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 17:36:27 by galambey          #+#    #+#             */
-/*   Updated: 2024/03/22 10:27:49 by garance          ###   ########.fr       */
+/*   Updated: 2024/03/22 11:23:02 by galambey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ int main( void ) {
 	int a = 2;
 	int b = 3;
 	
-	::swap( &a, &b ); // erreur sujet ? envoyer a swap pointeur
+	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	::swap(&c, &d);
+	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
--- C++_Module_07/ex02/srcs/main.cpp
+++ C++_Module_07/ex02/srcs/main.cpp
@@ -1,35 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: galambey <galambey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/21 17:37:10 by galambey          #+#    #+#             */
-/*   Updated: 2024/03/22 11:15:41 by galambey         ###   ########.fr       */
+/*   Created: 2024/03/22 13:38:23 by galambey          #+#    #+#             */
+/*   Updated: 2024/03/22 13:38:24 by galambey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WHATEVER_HPP
-# define WHATEVER_HPP
-
-template <class myType>
-myType max(myType a, myType b) {
-    return (a >= b ? a : b);
-}
-
-template <class myType>
-myType min(myType a, myType b) {
-    return (a <= b ? a : b);
-}
-
-template <class myType>
-void swap(myType & a, myType  & b) {
-    myType tmp;
-
-    tmp = a;
-    a = b;
-    b = tmp;
-}
-
-#endif
